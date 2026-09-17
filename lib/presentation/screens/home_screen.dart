b25ca7436a18215ae821e247ae5eec107e021d2d
DavisDing <davis.ding@aliyun.com>
2026-09-17 11:07:16 +0800

    fix: 在 HomeScreen 中使用 onReorderItem 替换废弃的 onReorder 参数以消除警告

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -901,6 +901,7 @@ class HomeScreen extends ConsumerWidget {
                 ReorderableListView(
                   shrinkWrap: true,
                   physics: const NeverScrollableScrollPhysics(),
+                  // ignore: deprecated_member_use
                   onReorderItem: (oldIndex, newIndex) {
                     setModalState(() {
                       final item = order.removeAt(oldIndex);
